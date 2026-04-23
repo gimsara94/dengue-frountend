1e19970ea1cffa7e36d1019f4afdcc7a68d96e11
gimsara94 <gimsarabulagala@gmail.com>
2026-04-23 19:59:17 +0530

    Setup GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminLayout from './components/AdminLayout';
 import AdminDashboard from './pages/AdminDashboard';
